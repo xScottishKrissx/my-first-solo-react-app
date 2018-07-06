6dc1d067d4740af8c1a0f1dea489c9398908b0e7
xScottishKrissx <chrisdunne66@gmail.com>
2018-07-06 12:28:19 +0100

    Day 16
    Let's try deploying this badboy using github pages

--- myfirstsoloapp/src/components/Header/index.js
+++ myfirstsoloapp/src/components/Header/index.js
@@ -4,11 +4,11 @@ import './index.css';
 const Header = () => {
     return (
         <div className="headerWrapper">
-            <h3>Cards Project.</h3>
+            <h3>Cards Project</h3>
             <p>the very first thing i ever did with the react.js framework.</p>
             <p>Components, Props, State, Stateless Components vs Class Based Components, Reusability, Getting a simple project up and running.</p>
 
-            <p>if this page looks empty when you're here then give the browser a refresh a few times. I am using an external service to provide me images and I have zero control over whether it works or not. You should see something like this(insert hyperlink here Christopher!!!)</p>
+            <p>if this page looks empty when you're here then give the browser a refresh a few times. I am using an external service to provide the images and I have zero control over whether or not it works. You should see something like this(insert hyperlink here Christopher!!!)</p>
             <ul>
                 <li>Random images generate every time the browser refreshes. This was to demonstrate that i could work with json data and arrays.</li>
                 <li>the same goes for the random author image</li>
